7f64b76a9b8a94f75140803f52a0e8b032fcb97d
Zach Baylin <me@zachbayl.in>
2021-05-11 16:17:19 -0400

    Esy: add empty build script

---
+++ esy/build.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+
+# Empty because we don't need to build libcurl itself.
+# On macOS, it is vendored w/ XCode/Clang
+# On Linux, we expect you to have installed it from your package manager
+# On Windows, we vendor a DLL
