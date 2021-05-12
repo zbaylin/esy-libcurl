02fb459af6d100a2179c7a7528289d94a40fae65
Zach Baylin <me@zachbayl.in>
2021-05-12 17:39:05 -0400

    Windows: use non-Cygwin prebuilt DLL

--- esy/install.sh
+++ esy/install.sh
@@ -11,11 +11,11 @@ if [ "$os" == "windows" ]; then
   cp -a $cur__root/curl/include/curl $cur__install/include/
 
   # Copy the DLL to the bin folder
-  cp -a $cur__root/prebuilt/windows/cygcurl-4.dll $cur__install/bin/
+  cp -a $cur__root/prebuilt/windows/libcurl-x64.dll $cur__install/bin/
 
   # Generate a definition file in the lib directory
-  esy/gendef.exe - $cur__install/bin/cygcurl-4.dll > $cur__install/lib/curl.def
+  esy/gendef.exe - $cur__install/bin/libcurl-x64.dll > $cur__install/lib/curl.def
 
   # Generate a lib file from the DLL
-  x86_64-W64-mingw32-dlltool.exe -D $cur__install/cygcurl-4.dll -d $cur__install/lib/curl.def -A -l $cur__install/lib/libcurl.a
+  x86_64-W64-mingw32-dlltool.exe -D $cur__install/libcurl-x64.dll -d $cur__install/lib/curl.def -A -l $cur__install/lib/libcurl.a
 fi
